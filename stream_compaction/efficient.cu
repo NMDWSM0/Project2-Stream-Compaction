03bebb167ecbb4fdf285562fefce5ff2538d0847
yu jiang <1526606449@qq.com>
2025-09-09 14:32:59 -0400

    complete efficient compaction

--- stream_compaction/efficient.cu
+++ stream_compaction/efficient.cu
@@ -81,6 +81,24 @@ namespace StreamCompaction {
             odata[index] += scanned_blocksumdata[blockIdx.x];
         }
 
+        __global__ void kernValueToBool(int N, int* odata, const int* idata) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index >= N) {
+                return;
+            }
+            odata[index] = (idata[index] > 0);
+        }
+
+        __global__ void kernDoCompaction(int N, int* odata, const int* idata, const int* indexdata) {
+            int index = threadIdx.x + (blockIdx.x * blockDim.x);
+            if (index >= N) {
+                return;
+            }
+            if (idata[index] > 0) {
+                odata[indexdata[index]] = idata[index];
+            }
+        }
+
         void recursiveExclusiveScan(int N, const std::vector<int*>& odatas, int bufferlvl) {
             if (N <= blockSize) 
             {
@@ -104,7 +122,6 @@ namespace StreamCompaction {
             // compute dims
             int depth = ilog2ceil(n);
             int N = 1 << depth;  // set N the smallest power of 2
-            int fullBlocksPerGrid = (N + blockSize - 1) / blockSize;
 
             // memory allocation
             std::vector<int*> dev_datas;
@@ -146,10 +163,54 @@ namespace StreamCompaction {
          * @returns      The number of elements remaining after compaction.
          */
         int compact(int n, int *odata, const int *idata) {
+            // compute dims
+            int depth = ilog2ceil(n);
+            int N = 1 << depth;  // set N the smallest power of 2
+            int fullBlocksPerGrid = (N + blockSize - 1) / blockSize;
+
+            // memory allocation
+            std::vector<int*> dev_datas;
+            int bufferSize = N;
+            do {
+                int* dev_data;
+                cudaMalloc(&dev_data, bufferSize * sizeof(int));
+                cudaMemset(dev_data, 0, bufferSize * sizeof(int));
+                dev_datas.push_back(dev_data);
+                bufferSize /= blockSize;
+            } while (bufferSize > 1);
+            int *dev_idata, *dev_odata;
+            cudaMalloc(&dev_idata, N * sizeof(int));
+            cudaMemset(dev_idata, 0, N * sizeof(int));
+            cudaMalloc(&dev_odata, N * sizeof(int));
+            cudaMemset(dev_odata, 0, N * sizeof(int));
+
+            // copy data to device
+            cudaMemcpy(dev_idata, idata, n * sizeof(int), cudaMemcpyHostToDevice);
+
+            // start kernel
             timer().startGpuTimer();
-            // TODO
+            // convert to bool
+            kernValueToBool<<<fullBlocksPerGrid, blockSize>>>(N, dev_datas[0], dev_idata);
+            // do exclusive scan
+            recursiveExclusiveScan(N, dev_datas, 0);
+            // write values
+            kernDoCompaction<<<fullBlocksPerGrid, blockSize>>>(N, dev_odata, dev_idata, dev_datas[0]);
             timer().endGpuTimer();
-            return -1;
+
+            // copy back data
+            // first copy back index data to find the count
+            cudaMemcpy(odata, dev_datas[0], N * sizeof(int), cudaMemcpyDeviceToHost);
+            int count = odata[N - 1];
+            // then copy real values
+            cudaMemcpy(odata, dev_odata, count * sizeof(int), cudaMemcpyDeviceToHost);
+
+            // free memory
+            for (auto dev_data : dev_datas) {
+                cudaFree(dev_data);
+            }
+            cudaFree(dev_idata);
+            cudaFree(dev_odata);
+            return count;
         }
     }
 }
